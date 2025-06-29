62e5e8cf48c2b2e9a373c9c0638bb3074f005dff
Disha1605-hash <disha.gode@gmail.com>
2025-06-29 21:56:50 +0530

    Update db.py

--- server/db.py
+++ server/db.py
@@ -1,10 +1,11 @@
-import mysql.connector
-import os
+import pymysql
 
 def get_connection():
-    return mysql.connector.connect(
-        host=os.environ['DB_HOST'],
-        user=os.environ['DB_USER'],
-        password=os.environ['DB_PASSWORD'],
-        database=os.environ['DB_NAME']
+    return pymysql.connect(
+        host="bdkhottcwh8m9x33k5u5-mysql.services.clever-cloud.com",
+        user="ua3lg4aaeojwygke",
+        password="NZY3XliGPSbgwErONdXg",  
+        database="bdkhottcwh8m9x33k5u5",
+        port=3306,
+        cursorclass=pymysql.cursors.DictCursor
     )
